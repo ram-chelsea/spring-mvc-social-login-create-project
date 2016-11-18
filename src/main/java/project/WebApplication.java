85aba2cff21a91e3cd690dab85e6a133409cd45a
ram-chelsea <ramchelsearamchelsea@gmail.com>
2016-11-18 14:48:16 +0300

    com1

--- src/main/java/project/WebApplication.java
+++ src/main/java/project/WebApplication.java
@@ -1,16 +1,21 @@
-package project.config;
+package project;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import java.io.File;
 
 
 @SpringBootApplication
 @EntityScan(basePackages = "project.dm")
 public class WebApplication extends SpringBootServletInitializer {
+
+    private static final String WEBAPP_DIR_LOCATION = "src/main/webapp";
+    private static final String CONTEXT_PATH = "";
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(WebApplication.class);
@@ -18,5 +23,10 @@ public class WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(WebApplication.class, args);
+        File webappDir = new File(WEBAPP_DIR_LOCATION);
+        Tomcat tomcat = new Tomcat();
+        tomcat.addWebapp(CONTEXT_PATH, webappDir.getAbsolutePath());
     }
+
+
 }
--- src/main/java/project/controller/UserController.java
+++ src/main/java/project/controller/UserController.java
@@ -5,16 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.dm.User;
 import project.security.UserValidator;
 import project.service.SecurityService;
 import project.service.UserService;
 
-@RestController
+@Controller
 public class UserController {
     @Autowired
     private UserService userService;
@@ -58,8 +55,16 @@ public class UserController {
         return "login";
     }
 
-    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/welcome"}, method = RequestMethod.GET)
     public String welcome(Model model) {
         return "welcome";
     }
-}
+
+    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    public
+    String index() {
+        return "index.html";
+    }
+
+  }
+
